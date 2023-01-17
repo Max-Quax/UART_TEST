c449d84fffdee9ce49a9202da5dca295330edfcd
Max <Max@DESKTOP-UQKCPL2>
2023-01-17 10:06:15 -0500

    Table

--- UART_Test/DAD_UART.c
+++ UART_Test/DAD_UART.c
@@ -9,8 +9,18 @@
 
 uint8_t DAD_UART_Find_Second_Mod_Reg(float divisionFactor){
     //TODO lookup table
-    float
+    float fract = divisionFactor - ((int)divisionFactor);
+    float table[] = {0.0000, 0.0529, 0.0715, 0.0835, 0.1001, 0.1252, 0.1430, 0.1670, 0.2147, 0.2224, 0.2503, 0.3000, 0.3335, 0.3575, 0.3753, 0.4003, 0.4286, 0.4378,
+                     0.5002, 0.5715, 0.5715, 0.6003, 0.6254, 0.6432, 0.6667, 0.7001, 0.7147, 0.7503, 0.7861, 0.8004, 0.8333, 0.8464, 0.8572, 0.8751, 0.9004, 0.9170, 0.9288, 1};
 
+    if(divisionFactor == 0)
+        return 0;
+    if(divisionFactor == 1)
+        return 1;
+    for(uint8_t i = 0; i <= 0xFF; i++){
+        if(divisionFactor > table[i])
+            return i - 1;
+    }
 }
 
 void DAD_UART_Set_Config(uint16_6 baudRate, eUSCI_UART_ConfigV1* uartConfig){
@@ -31,7 +41,27 @@ void DAD_UART_Set_Config(uint16_6 baudRate, eUSCI_UART_ConfigV1* uartConfig){
 }
 
 void DAD_UART_Init(uint32_t moduleInstance, eUSCI_UART_ConfigV1* config){
-    UART_initModule(EUSCI_A0_BASE, &uartConfig);
-    UART_enableModule(EUSCI_A0_BASE);
+    UART_initModule(moduleInstance, &uartConfig);
+    UART_enableModule(moduleInstance);
+
+    //Which interrupt is it
+    uint32_t interrupt;
+    switch(moduleInstance){
+    case:EUSCI_A0_BASE
+            interrupt = INT_EUSCIA0;
+    case:EUSCI_A1_BASE
+            interrupt = INT_EUSCIA1;
+    case:EUSCI_A2_BASE
+            interrupt = INT_EUSCIA2;
+    case:EUSCI_A3_BASE
+            interrupt = INT_EUSCIA3;
+    break;
+
+    //Enable Interrupts
+    MAP_UART_enableInterrupt(moduleInstance, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    MAP_Interrupt_enableInterrupt(interrupt);
+    MAP_Interrupt_enableSleepOnIsrExit();
+    MAP_Interrupt_enableMaster();
+    }
 }
 
